6eee5f654b98172adfd99c9d0276446f98f59608
serz999 <serz99992@gmail.com>
2023-12-04 22:31:16 +0300

    feat: add phone normalization & validation

--- pkg/phone.go
+++ pkg/phone.go
@@ -1,6 +1,32 @@
 package pkg
 
-func PhoneNormalize(phone string) (normalizedPhone string, err error) {
+import (
+	"errors"
+	"strings"
+)
 
-    return phone, nil
+func PhoneNormalize(phone string) (normalizedPhone string, err error) {
+    var sb strings.Builder
+	for i := range phone {
+		n := phone[i]
+		if n >= '0' && n <= '9' {
+			sb.WriteByte(n)
+		}
+	}
+	normalizedPhone = sb.String()
+
+	if sb.Len() == 0 {
+		return "", errors.New("Invalid phone number")
+	}
+	if normalizedPhone[0] == '8' {
+		normalizedPhone = "7" + normalizedPhone[1:]
+	}
+	if normalizedPhone[0] != '7' {
+		return "", errors.New("Invalid phone number")
+	}
+	if len(normalizedPhone) != 11 {
+		return "", errors.New("Invalid phone number")
+	}
+
+	return normalizedPhone, nil
 }
